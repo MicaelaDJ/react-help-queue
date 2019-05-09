6def35023189da81c61b26580a55b42671036648
Student <example@example.com>
2019-05-09 16:45:20 -0700

    work on redux actions and updates

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,18 +1,28 @@
 export default (state = {}, action) => {
-    switch (action.type) {
-    case 'ADD_TICKET':
-      const { names, location, issue, timeOpen, id } = action;
-      let newState = Object.assign({}, state, {
-        [id]: {
-          names: names,
-          location: location,
-          issue: issue,
-          timeOpen: timeOpen,
-          id: id
-        }
-      });
-      return newState;
-    default:
-      return state;
-    }
-  };
+  let newState;
+  const { names, location, issue, timeOpen, id, formattedWaitTime } = action;
+
+  switch (action.type) {
+  case 'ADD_TICKET':
+    newState = Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id
+      }
+    });
+    return newState;
+
+  case 'UPDATE_TIME':
+  const newTicket = Object.assign({}, state[id], {formattedWaitTime});
+  newState = Object.assign({}, state, {
+    [id]: newTicket
+  });
+    return newState;
+
+  default:
+    return state;
+  }
+};
